3e60f59b1ad451193486d6cbaaa8e5060cb9db5b
Chedvah-b <58727484+Chedvah-b@users.noreply.github.com>
2021-06-28 19:29:16 +0300

    second commit

--- client/src/app/suggestions/suggestions.component.ts
+++ client/src/app/suggestions/suggestions.component.ts
@@ -9,12 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class SuggestionsComponent implements OnInit {
 
   constructor() { }
-  data=[{img:'https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image-p-1080.jpeg' , subject:'פחים שרופים', street: "כף החיים",likes:5,dislikes:5},
-  {img:'https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image-p-1080.jpeg' , subject:'פחים שרופים', street: "כף החיים",likes:5,dislikes:5},
-  {img:'https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image-p-1080.jpeg', subject:'פחים שרופים', street: "כף החיים",likes:5,dislikes:5},
-  {img:'https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image-p-1080.jpeg', subject:'פחים שרופים', street: "כף החיים",likes:5,dislikes:5},
-  {img:'https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image-p-1080.jpeg', subject:'פחים שרופים', street: "כף החיים",likes:5,dislikes:5},
-  {img:'https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image-p-1080.jpeg', subject:'פחים שרופים', street: "כף החיים",likes:5,dislikes:5}]
+  data=[{img:'https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image-p-1080.jpeg' , subject:'פחים שרופים', street: "כף החיים",likes:5,dislikes:10},
+  {img:'https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image-p-1080.jpeg' , subject:'פחים שרופים', street: "כף החיים",likes:5,dislikes:10},
+  {img:'https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image-p-1080.jpeg', subject:'פחים שרופים', street: "כף החיים",likes:5,dislikes:10},
+  {img:'https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image-p-1080.jpeg', subject:'פחים שרופים', street: "כף החיים",likes:5,dislikes:10},
+  {img:'https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image-p-1080.jpeg', subject:'פחים שרופים', street: "כף החיים",likes:5,dislikes:10},
+  {img:'https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image-p-1080.jpeg', subject:'פחים שרופים', street: "כף החיים",likes:5,dislikes:10}]
   ngOnInit(): void {
     //const data=get();
 
